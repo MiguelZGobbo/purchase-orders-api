4451f1167959c360c47c426331491e8d31b932f1
Miguel Zager Gobbo <miguelzgobbo@gmail.com>
2025-03-13 15:08:34 -0300

    get purchase ordes items

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -30,8 +30,29 @@ def get_purchase_orders_by_id(id):
          if po["id"] == id:
              return jsonify(purchase_orders)
     return ("Id não encontrado")
-         
-     
+
+@app.route('/purchase_orders', methods = ['POST'])
+def create_get_purchase_order():
+    request_data = request.get_json()
+    purchase_order = {
+        'id': request_data['id'],
+        'description': request_data['description'],
+        'items': []
+    }
+
+    purchase_orders.append(purchase_order)
+
+    return jsonify(purchase_order)
+
+@app.route('/purchase_orders/int<id>/items')      
+def get_purchase_orders_items(id):
+    for po in purchase_orders:
+        if po['id'] == id:
+            return jsonify(po['items'])
+        
+    return ("Id não encontrado")
+    
+      
 
 app.run(port=5000)
 

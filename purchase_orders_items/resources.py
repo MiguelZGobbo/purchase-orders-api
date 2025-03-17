aad3ac393122dbdf8250e7fd3bc8dd83059cda38
Miguel Zager Gobbo <miguelzgobbo@gmail.com>
2025-03-17 13:56:03 -0300

    post_purchase_orders_items

--- purchase_orders_items/resources.py
+++ purchase_orders_items/resources.py
@@ -1,4 +1,4 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 from flask import jsonify
 
 purchase_orders = [
@@ -16,8 +16,48 @@ purchase_orders = [
 ]
 
 class PurchaseOrdersItems(Resource):
+    parser = reqparse.RequestParser()
+
+    parser.add_argument(
+        'id',
+        type = int,
+        required = True,
+        help = "Informe um ID"
+    )
+
+    parser.add_argument(
+        'description',
+        type =  str,
+        required = True,
+        help = "Informe uma descrição"
+    )
+
+    parser.add_argument(
+        'price',
+        type = float,
+        required = True,
+        help = "Informe o preço"
+    )
+
     def get(self, id):
         for po in purchase_orders:
             if po['id'] == id:
                 return jsonify(po["items"])
         return ("Id não encontrado!")
+    
+    def post(self, id):
+        request_data = PurchaseOrdersItems().parser.parse_args()
+
+        for po in purchase_orders:
+            if po['id'] == id:
+                po['items'].append({
+                    'id': request_data['id'],
+                    'description': request_data['description'],
+                    'price': request_data['price']
+                })
+
+                return jsonify(po)       
+                
+        return ("Id não encontrado")
+
+    

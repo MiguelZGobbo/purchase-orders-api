e4c628a2dc3cb147054445cb245444a7f009b78c
Miguel Zager Gobbo <miguelzgobbo@gmail.com>
2025-04-03 15:55:16 -0300

    conftest para test_purchase_orders

--- tests/purchase_orders/test_resources.py
+++ tests/purchase_orders/test_resources.py
@@ -1,10 +1,11 @@
 import json
 
-def test_get_purchase_orders(test_client):
+def test_get_purchase_orders(test_client, seed_db):
     response = test_client.get('/purchase_orders')
 
     assert response.status_code == 200
-    assert response.json[0]['id'] == 1
+    assert response.json[0]['id'] == seed_db.id
+    assert response.json[0]['description'] == seed_db.description
 
 def test_post_purchase_orders(test_client):
     obj = {'id': 2, 'description': ''}

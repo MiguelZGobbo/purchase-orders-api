cea88fdecfb7f706cc1adf45ff836ad25234893e
Miguel Zager Gobbo <miguelzgobbo@gmail.com>
2025-08-13 15:48:36 -0300

    docs(exceptions): adicionando docstrings e explicações nos testes

--- exceptions/exceptions.py
+++ exceptions/exceptions.py
@@ -1,5 +1,14 @@
+"""Exceções personalizadas para o gerenciamento de erros da aplicação."""
+
 from werkzeug.exceptions import HTTPException
 
 
 class QuantityException(HTTPException):
+    """
+    Exceção levantada quando a quantidade fornecida é inválida.
+
+    Define o código HTTP 400 para indicar uma requisição inválida, geralmente
+    associada a quantidades que excedem limites ou são inadequadas no contexto
+    do pedido de compra.
+    """
     code = 400
